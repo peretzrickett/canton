da498f71dd711dacb2cb5d587aad098b0217239d
Peretz Rickett <peretz@rickett.us>
2024-02-28 20:15:52 +0200

    updated and tested

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "canton-resources"
   location = "East US"
